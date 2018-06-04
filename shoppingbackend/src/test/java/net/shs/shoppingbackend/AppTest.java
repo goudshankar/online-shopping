7c1784ccb94487d58fef494601ea0a3a14e9c698
shankergoudedgi <shankar.jse@outlook.com>
2018-06-04 22:15:05 +0530

    My Project setup completed successfully

---
+++ shoppingbackend/src/test/java/net/shs/shoppingbackend/AppTest.java
@@ -0,0 +1,20 @@
+package net.shs.shoppingbackend;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
